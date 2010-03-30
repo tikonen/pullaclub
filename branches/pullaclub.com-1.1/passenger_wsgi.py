51bf8807551df4066e05662647001b8f8b4d2841
pullaclub <pullaclub@4b828fdf-fd43-4e4a-b492-4c486c2da95d>
2010-03-30 17:50:22 +0000

    cleanup

--- branches/pullaclub.com-1.1/passenger_wsgi.py
+++
@@ -1,6 +0,0 @@
-import sys, os
-sys.path.append(os.getcwd())
-sys.path.append(os.path.join(os.getcwd(),"pullaclub"))
-os.environ['DJANGO_SETTINGS_MODULE'] = "pullaclub.settings"
-import django.core.handlers.wsgi
-application = django.core.handlers.wsgi.WSGIHandler()
